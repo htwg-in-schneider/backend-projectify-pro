1dd9408486573075cbe3127b2e0e66cbd39192ad
Your Name <you@example.com>
2025-12-01 21:28:57 +0100

    deleted test data and unused files

--- src/main/java/de/htwg/in/schneider/saitenweise/backend/controller/CategoryController.java
+++ src/main/java/de/htwg/in/schneider/saitenweise/backend/controller/CategoryController.java
@@ -6,8 +6,6 @@ import de.htwg.in.schneider.saitenweise.backend.model.Category;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/category")
@@ -16,11 +14,5 @@ public class CategoryController {
     @GetMapping()
     public List<Category> getCategories() {
         return Arrays.asList(Category.values());
-    }
-
-    @GetMapping("/translation")
-    public Map<String, String> getAllCategories() {
-        return Arrays.stream(Category.values())
-                .collect(Collectors.toMap(Category::name, Category::getGermanName));
     }
 }
