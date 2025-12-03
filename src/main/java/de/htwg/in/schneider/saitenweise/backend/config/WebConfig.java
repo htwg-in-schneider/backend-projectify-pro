f9f72d2c4c8f1c500bcf979cb074a888a3a0288d
katarinastevanovic7 <katarina.stevanovic7676@gmail.com>
2025-12-03 23:28:43 +0100

    final

--- src/main/java/de/htwg/in/schneider/saitenweise/backend/config/WebConfig.java
+++ src/main/java/de/htwg/in/schneider/saitenweise/backend/config/WebConfig.java
@@ -1,17 +1,19 @@
 package de.htwg.in.schneider.saitenweise.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
